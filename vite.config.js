0de97cccf81807243208e0f4b1533de822a16bbf
Farruh4eg <farruh4eg@gmail.com>
2024-06-06 21:37:02 +0300

    add favicon

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  optimizeDeps: {
-    include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'],
-  },
 };
 
 export default config;
